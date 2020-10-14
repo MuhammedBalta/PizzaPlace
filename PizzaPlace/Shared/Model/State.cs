32bf34d09c98790323a71574cbe6a5cc61355262
Muhammed Balta <muhammed.balta@hotmail.com>
2020-10-15 00:14:46 +0300

    Add project files.

---
+++ PizzaPlace/Shared/Model/State.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaPlace.Shared.Model
+{
+    public class State
+    {
+        public Menu Menu { get; set; } = new Menu(); 
+        public Basket Basket { get; set; } = new Basket();
+        public UI UI { get; set; } = new UI();
+    }
+}
